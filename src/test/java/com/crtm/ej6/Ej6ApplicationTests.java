5441f9e818af212b1b5f844728e2c8c75d7cfec3
racena <racena@indra.es>
2024-09-18 13:07:30 +0200

    Add files via upload

---
+++ src/test/java/com/crtm/ej6/Ej6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crtm.ej6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
